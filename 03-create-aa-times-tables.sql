33bf63816b7bcd9d8917ad68f2ee8e8e5dfccba0
Dillon Long <dplong@crimson.ua.edu>
2020-07-20 16:53:39 -0700

    Completed implementing each file. Each file passes the test specs.

--- 03-create-aa-times-tables.sql
+++ 03-create-aa-times-tables.sql
@@ -12,7 +12,10 @@ SET ROLE aa_times;
 -- | last_name   | VARCHAR(50)  | NOT NULL    |
 -- | email       | VARCHAR(100) | NOT NULL    |
 
-
+CREATE TABLE people (id SERIAL PRIMARY KEY,
+                    first_name VARCHAR(50) NOT NULL,
+                    last_name VARCHAR(50) NOT NULL,
+                    email VARCHAR(100) NOT NULL);
 
 -- The "sections" table
 -- | Column name | Type         | Constraints |
@@ -20,7 +23,8 @@ SET ROLE aa_times;
 -- | id          | SERIAL       | PRIMARY KEY |
 -- | name        | VARCHAR(150) | NOT NULL    |
 
-
+CREATE TABLE sections (id SERIAL PRIMARY KEY,
+                    name VARCHAR(150) NOT NULL);
 
 -- The "stories" table
 -- | Column name | Type    | Constraints                             |
@@ -29,3 +33,10 @@ SET ROLE aa_times;
 -- | author_id   | INTEGER | FOREIGN KEY to people table, NOT NULL   |
 -- | content     | TEXT    | NOT NULL                                |
 -- | section_id  | INTEGER | FOREIGN KEY to sections table, NOT NULL |
+
+CREATE TABLE stories (id SERIAL PRIMARY KEY,
+                    author_id INTEGER NOT NULL,
+                    content text NOT NULL,
+                    section_id INTEGER NOT NULL,
+                    FOREIGN KEY (author_id) REFERENCES people (id),
+                    FOREIGN KEY (section_id) REFERENCES sections (id));
